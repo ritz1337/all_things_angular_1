f2b921606bb252452ffa9e0e064e4ffdd168e899
Ritwik Ruia <ritwikruia@gmail.com>
2017-02-23 12:14:23 -0800

    adds till before Your Turn

---
+++ angular_nested_controller_demo/intro_controllers/js/controllers/CarController.js
@@ -0,0 +1,8 @@
+angular.module("IntroAngular")
+    .controller("CarController", CarController)
+
+function CarController () {
+  var vm = this;
+
+  vm.test = "Goodbye World";
+}
---
+++ angular_nested_controller_demo/intro_controllers/js/controllers/HomeController.js
@@ -0,0 +1,13 @@
+// js/controllers/HomeController.js
+
+angular.module("IntroAngular")
+    .controller("HomeController", HomeController)
+
+function HomeController () {
+  var vm = this;
+    vm.test = "Hello World"
+
+    vm.count = 5
+
+
+}
