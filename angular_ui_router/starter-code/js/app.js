16c015da742c197e17610ca019691b04d5e740fe
Ritwik Ruia <ritwikruia@gmail.com>
2017-02-24 15:05:31 -0800

    adds solution

--- angular_ui_router/starter-code/js/app.js
+++ angular_ui_router/starter-code/js/app.js
@@ -1,5 +1,23 @@
 (function() {
 	'use strict';
 
-	angular.module('todoApp', []);
+	angular.module('todoApp', ['ui.router'])
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
+     .state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+     }).state('archive', {
+        url: '/archive',
+        templateUrl: 'archive.html'
+     }).state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+     });
+
+     $urlRouterProvider.otherwise('/');
+  }
 }());
